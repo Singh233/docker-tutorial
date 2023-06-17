3fa5426282d9bedf7c497996c6202ae7421008e8
Sanambir Singh <Sanambir123@gmail.com>
2023-06-17 12:47:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/', (req, res ) => 
+    res.json({ message: 'Docker is easy 🐳', env: process.env.TESTING }) 
+);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
